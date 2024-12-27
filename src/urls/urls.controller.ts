9df73ed1c041235afcb738d81927b73ba5af8172
Bruno Passos <brunopassos_bp@hotmail.com>
2024-12-27 20:07:31 -0300

    feat: implementa o crud de urls

--- src/urls/urls.controller.ts
+++ src/urls/urls.controller.ts
@@ -1,7 +1,6 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Res } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { UrlsService } from './urls.service';
 import { CreateUrlDto } from './dto/create-url.dto';
-import { Response } from 'express';
 
 @Controller('urls')
 export class UrlsController {
