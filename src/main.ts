8c04f9be7d53b9595507dd9dcdd76bdbe7cd2e3a
Bruno Passos <brunopassos_bp@hotmail.com>
2024-12-29 12:44:54 -0300

    feat: implementa logs com sentry

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,20 @@
 import 'dotenv/config';
-// Import with `const Sentry = require("@sentry/nestjs");` if you are using CJS
-import * as Sentry from "@sentry/nestjs"
-import { nodeProfilingIntegration } from "@sentry/profiling-node";
-
-Sentry.init({
-  dsn: process.env.SENTRY_DSN,
-  integrations: [
-    nodeProfilingIntegration(),
-  ],
-  // Tracing
-  tracesSampleRate: 1.0, //  Capture 100% of the transactions
-});
-// Manually call startProfiler and stopProfiler
-// to profile the code in between
-Sentry.profiler.startProfiler();
-
-// Starts a transaction that will also be profiled
-Sentry.startSpan({
-  name: "My First Transaction",
-}, () => {
-  // the code executing inside the transaction will be wrapped in a span and profiled
-});
-
-// Calls to stopProfiling are optional - if you don't stop the profiler, it will keep profiling
-// your application until the process exits or stopProfiling is called.
-Sentry.profiler.stopProfiler();
 import { NestFactory } from '@nestjs/core';
+import * as Sentry from '@sentry/node';
+import { nodeProfilingIntegration } from '@sentry/profiling-node';
 import { AppModule } from './app.module';
+import { SentryInterceptor } from './common/interceptors/sentry.interceptor';
 
 async function bootstrap() {
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    integrations: [nodeProfilingIntegration()],
+    tracesSampleRate: 1.0,
+  });
+
   const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(new SentryInterceptor());
+
   await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
