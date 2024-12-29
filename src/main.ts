52327c1b330f9592b36aa75d64b4fcaf0ed603d1
Bruno Passos <brunopassos_bp@hotmail.com>
2024-12-29 12:45:18 -0300

    feat: implementa logs com sentry

--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,21 @@ import { AppModule } from './app.module';
 import { SentryInterceptor } from './common/interceptors/sentry.interceptor';
 
 async function bootstrap() {
-  Sentry.init({
-    dsn: process.env.SENTRY_DSN,
-    integrations: [nodeProfilingIntegration()],
-    tracesSampleRate: 1.0,
-  });
+  const isSentryEnabled = process.env.SENTRY_ENABLED === 'true';
+
+  if (isSentryEnabled) {
+    Sentry.init({
+      dsn: process.env.SENTRY_DSN,
+      integrations: [nodeProfilingIntegration()],
+      tracesSampleRate: 1.0,
+    });
+  }
 
   const app = await NestFactory.create(AppModule);
-  app.useGlobalInterceptors(new SentryInterceptor());
+
+  if (isSentryEnabled) {
+    app.useGlobalInterceptors(new SentryInterceptor());
+  }
 
   await app.listen(3000, '0.0.0.0');
 }
